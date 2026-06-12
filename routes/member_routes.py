c026d29f51640c9c1be38e8952691059f5abcba7
Pesach Bagad <bgdpsh@gmail.com>
2026-06-12 13:54:36 +0300

    Add get_all_members function

--- routes/member_routes.py
+++ routes/member_routes.py
@@ -15,4 +15,10 @@ def add_member(data:AddMember):
     new_data=data.model_dump()
     result=member.create_member(new_data)
     return {"created member successfully, new id is": result}
-@router
+
+@router.get("/members")
+def get_members():
+    result=member.get_all_members()
+    if not result:
+        raise HTTPException(status_code=404,detail="Error the members tabel was empty, no member founds")
+    return result
